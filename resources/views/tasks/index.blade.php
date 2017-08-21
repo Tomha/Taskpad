eb8126c0554cfed17b03a5389ad2fd1b1795c97a
Tomha <tomhah@protonmail.com>
2017-08-21 16:51:59 +1200

    Added initial graph display - currently is static only

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -31,6 +31,14 @@
 	}
 
 	#create-task {
+		margin-bottom: 10px;
+	}
+
+	#burndown-box {
+		display: block;
+		margin: auto;
+		max-width: 500px;
+		max-height: 200px;
 		margin-bottom: 50px;
 	}
 
@@ -56,4 +64,53 @@
 
 	<a href="/tasks/create" class="btn btn-lg btn-primary" id='create-task'>Add Task</a>
 
+	<div id="burndown-box">
+		<canvas id="burndown-graph"></canvas>
+	</div>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js"></script>
+	<script>
+		var ctx = document.getElementById("burndown-graph").getContext('2d');
+		var myChart = new Chart(ctx, {
+			type: 'line',
+			data: {
+				labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
+						21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,
+						41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60],
+				datasets: [{
+					data: [20,19,18,14,13,14,15,16,14,13,12,13,21,21,20,20,20,19,19,19],
+				}]
+			},
+			options: {
+				elements: {
+					line: {
+						tension: 0,
+					},
+					point: {
+						radius: 0,
+					}
+				},
+		        title: {
+					display: true,
+					text: 'Remaining Items (Past Hour)',
+					fontSize: 24
+				},
+				legend: {
+					display: false
+				},
+				scales:
+				{
+					xAxes: [{
+						display: false
+					}],
+					yAxes: [{
+						display: true,
+						ticks: {
+							beginAtZero: true
+						}
+					}]
+				}
+			}
+		});
+	</script>
 @endsection
